fe56d5724e16d3839fe4fc8cf8aba8fee78ef091
Sander Pajusalu <Sander.Pajusalu@yale.edu>
2019-01-15 15:59:11 -0500

    small change

--- gatk4_multisample/launch_cromwell_jointgt.sh
+++ gatk4_multisample/launch_cromwell_jointgt.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=64000
 
 java -Dconfig.file=/gpfs/ycga/project/ysm/lek/shared/tools/cromwell_wdl/slurm.conf -jar \
-/gpfs/ycga/project/ysm/lek/shared/tools/cromwell_wdl/cromwell-36.jar run \
+/gpfs/ycga/project/ysm/lek/shared/tools/jars/cromwell-36.jar run \
 /gpfs/ycga/project/ysm/lek/shared/tools/cromwell_wdl/gatk4_multisample/Multisample_jointgt_GATK4.wdl \
 -i /gpfs/ycga/project/ysm/lek/shared/tools/cromwell_wdl/gatk4_multisample/Multisample_jointgt_GATK4_inputs.json \
 -o /gpfs/ycga/project/ysm/lek/shared/tools/cromwell_wdl/gatk4_multisample/cromwell.options
